bf28b957c33001d97987d1edcc6374386e807de9
Krishna-hivoco <krishna@hivoco.com>
2024-07-26 13:27:13 +0530

    New commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Home from "./pages/Home";
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Interaction from "./pages/Interaction";
@@ -16,6 +17,7 @@ function App() {
         </Route>
       </Routes>
     </BrowserRouter>
+
   );
 }
 
---
+++ src/pages/Splash.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Splash() {
+  return (
+    <div className="bg-[url('/gif/bg-gif.gif')] bg-cover bg-no-repeat h-full md:rounded-2xl p-6">
+        
+    </div>
+  );
+}
+
+export default Splash;
