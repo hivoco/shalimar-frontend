0eec7258ccd82463ff06b25ffaa488b1a2b9f147
ranjanhivoco <ranjan@hivoco.com>
2024-09-16 16:39:40 +0530

    revamped home.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,28 +1,6 @@
-import { useEffect, useState } from "react";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 function Layout() {
-  // const [windowHeight, setWindowHeight] = useState(window.innerHeight);
-
-  // const { pathname } = useLocation();
-
-  // const isScrollRequired =
-  //   pathname === "/get-your-nearest-dealers" ||
-  //   pathname === "/enter-your-location" ||
-  //   pathname === "/get-your-nearest-painters";
-
-  // useEffect(() => {
-  //   const handleResize = () => {
-  //     setWindowHeight(window.innerHeight);
-  //   };
-
-  //   window.addEventListener("resize", handleResize);
-
-  //   return () => {
-  //     window.removeEventListener("resize", handleResize);
-  //   };
-  // }, []);
-
   return (
     <div className="bg-[url('/bgs/1.png')] md:w-80 md:my-1 md:rounded-2xl  w-full h-svh  scrollbar-hide md:h-[98vh] bg-cover bg-center bg-no-repeat  md:mx-auto">
       <Outlet />
