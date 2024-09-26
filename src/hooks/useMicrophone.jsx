1f047bb607e2c45038dfb049640d13800812579a
Krishna-hivoco <krishna@hivoco.com>
2024-09-26 18:07:21 +0530

    Sound is currently 0.05

--- src/hooks/useMicrophone.jsx
+++ src/hooks/useMicrophone.jsx
@@ -5,8 +5,8 @@ export const useMicrophone = () => {
   const micOpenSound = useRef(new Audio("/sounds/startmic.wav")); // Replace with correct path
   const micOffSound = useRef(new Audio("/sounds/endmic.wav")); // Replace with correct path
   const mediaStreamRef = useRef(null);
-  micOpenSound.current.volume = 0.1;
-  micOffSound.current.volume = 0.1;
+  micOpenSound.current.volume = 0.05;
+  micOffSound.current.volume = 0.05;
 
   // Function to open the microphone
   const openMic = async () => {
